f9eeb58de27f6a12d6e70fabd08a9b03bfffe5e3
pcunning-rh <pcunning@redhat.com>
2025-04-14 20:19:41 -0400

    Update webhook.py
    More datatypes, more fields, more fun

--- files/webhook.py
+++ files/webhook.py
@@ -3,17 +3,26 @@ import requests
 webhook_url = '' # EventStream URL
 auth_token = ''  # Authentication token from Event Stream Token Credential Type
 
-
 data = {
-    'event': 'user_created',
-    'message': 'Sample message content from service',
+    'event': 'journey_logged_in',
+    'message': 'Just a small-town dev, loggin’ in to a big-time system.',
     'user': {
-        'user_id': 12345,
-        'first_name': 'John',
-        'last_name': 'Doe',
-        'email': 'john.doe@example.com'
+        'user_id': 1981,
+        'first_name': 'Steve',
+        'last_name': 'Perrynix',
+        'email': 'steve.perrynix@journey.linux'
+    },
+    'terminal': 'tty7',
+    'login_method': 'ssh',
+    'location': 'South Detroit',
+    'auth_status': 'faithfully_authenticated',
+    'midnight_train': {
+        'destination': 'anywhere',
+        'departure_time': '00:00',
+        'dnf_payload': 'install_dreams'
     }
 }
+
 headers = {
     'Content-Type': 'application/json',
     'Authorization': f'Bearer {auth_token}'  # Authorization header
